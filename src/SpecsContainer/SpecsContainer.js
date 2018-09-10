bd67ff140eb196c631e86b74d60252ab17ffa6e6
John Forster <j_forster@hotmail.co.uk>
2018-09-10 16:55:49 +0100

    EDIT SpecsContainer to pass props down to ColorSelector

--- src/SpecsContainer/SpecsContainer.js
+++ src/SpecsContainer/SpecsContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './SpecsContainer.css'
+import {ColorSelector} from "../ColorSelector/ColorSelector";
 
 export function SpecsContainer(props) {
   return (
@@ -7,6 +8,11 @@ export function SpecsContainer(props) {
       <h1>{props.groupName}</h1>
       <p>Rating: {props.rating}</p>
       <p>{props.device.displayDescription}</p>
+      <ColorSelector
+        colors={props.phoneColors}
+        currentColor={props.device.colourName}
+        onSquareClick={props.selectColor}
+      />
     </div>
   )
 }
--- src/SpecsContainer/SpecsContainer.test.js
+++ src/SpecsContainer/SpecsContainer.test.js
@@ -2,10 +2,24 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import {SpecsContainer} from "./SpecsContainer";
 
+jest.mock('../ColorSelector/ColorSelector', () => ({
+  ColorSelector: 'mockColorSelector'
+}));
+
 describe('SpecsContainer', () => {
-  const groupName = 'TestPhone 1', rating = '3.3', description = 'This is a test phone';
-  const activeDevice = { displayDescription: description };
-  const specsContainer = shallow(<SpecsContainer groupName={groupName} rating={rating} device={activeDevice}/>);
+  const mockFunction = jest.fn();
+  const groupName = 'TestPhone 1', rating = '3.3', description = 'This is a test phone', colourName = 'testRed';
+  const activeDevice = { displayDescription: description, colourName: colourName };
+  const colors = ['#f00','#0f0','#00f'];
+  const specsContainer = shallow(
+    <SpecsContainer
+      groupName={groupName}
+      rating={rating}
+      device={activeDevice}
+      phoneColors={colors}
+      selectColor={mockFunction}
+    />
+  );
 
 
   it('renders the group name', () => {
@@ -19,4 +33,14 @@ describe('SpecsContainer', () => {
   it('renders the rating', () => {
     expect(specsContainer.children().contains(<p>Rating: {rating}</p>))
   });
+
+  it('renders a ColorSelector component', () => {
+    expect(specsContainer.children().contains(
+      <mockColorSelector
+        colors={colors}
+        currentColor={colourName}
+        onSquareClick={mockFunction}
+      />
+    ))
+  })
 });
