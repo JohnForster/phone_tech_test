810ea045bb39be31fc98c04a5b509097ef8c1e7a
John Forster <j_forster@hotmail.co.uk>
2018-09-10 12:46:44 +0100

    ADD css files for ImageContainer and OfferContainer

--- src/ImageContainer/ImageContainer.js
+++ src/ImageContainer/ImageContainer.js
@@ -1,6 +1,11 @@
 import React from "react";
+import './ImageContainer.css'
 
 export function ImageContainer(props) {
   const image = props.device.merchandisingMedia.find((image) => image.id === props.imageId);
-  return <img src={image.value} alt={props.device.displayName}/>
+  return(
+    <div className='image-container'>
+      <img className='display-image' src={image.value} alt={props.device.displayName}/>
+    </div>
+  )
 }
--- src/OfferContainer/OfferContainer.js
+++ src/OfferContainer/OfferContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { ImageContainer } from '../ImageContainer/ImageContainer.js'
+import './OfferContainer.css'
 
 export class OfferContainer extends Component {
   constructor(props){
@@ -15,7 +16,7 @@ export class OfferContainer extends Component {
   render() {
     const imageId = "PR_IMAGE_URLS_THUMBS_FRONT";
     return (
-      <div>
+      <div className='offer-container'>
         {this.state.groupName}
         <ImageContainer device={this.state.activeDevice} imageId={imageId}/>
       </div>
