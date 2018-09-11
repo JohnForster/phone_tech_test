de4df82be3c9f7a6658bb88b86d5ce4540bf08f8
John Forster <j_forster@hotmail.co.uk>
2018-09-11 12:10:12 +0100

    UPDATE testPhoneData for priceContainer

--- src/testPhoneData.js
+++ src/testPhoneData.js
@@ -9,7 +9,35 @@ export const testPhoneData = {
       "merchandisingMedia": [
         {"id": "test_image_id", "value": "test_image_path"}
       ],
-      "memory": "10GB"
+      "memory": "10GB",
+      "priceInfo": {
+        "bundlePrice": {
+          "bundleId": "110368",
+          "monthlyPrice": {
+            "gross": "43.20",
+            "net": "36.00",
+            "vat": "7.20"
+          },
+          "monthlyDiscountPrice": {
+            "gross": null,
+            "net": null,
+            "vat": null
+          }
+        },
+        "hardwarePrice": {
+          "hardwareId": "094351",
+          "oneOffPrice": {
+            "gross": "149",
+            "net": "124.17",
+            "vat": "24.83"
+          },
+          "oneOffDiscountPrice": {
+            "gross": "129",
+            "net": "107.50",
+            "vat": "21.50"
+          }
+        }
+      }
     },
     {"deviceId":"test1b",
       "displayName": "testPhone1b",
