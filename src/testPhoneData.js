4e4c2dffd93eae7d7371c6fa61b474710046061c
John Forster <j_forster@hotmail.co.uk>
2018-09-11 11:31:47 +0100

    MINOR css changes to ColorSelector and Image Container. Included missing phonedata json

--- src/testPhoneData.js
+++ src/testPhoneData.js
@@ -8,7 +8,8 @@ export const testPhoneData = {
       "colourHex": "#ff0000",
       "merchandisingMedia": [
         {"id": "test_image_id", "value": "test_image_path"}
-      ]
+      ],
+      "memory": "10GB"
     },
     {"deviceId":"test1b",
       "displayName": "testPhone1b",
@@ -16,6 +17,27 @@ export const testPhoneData = {
       "colourHex": "#00ff00",
       "merchandisingMedia": [
         {"id": "test_image_idb", "value": "test_image_pathb"}
-      ]}
+      ],
+      "memory": "10GB"
+    },
+    {
+      "deviceId": "test2a",
+      "displayName": "testPhone1",
+      "colourName": "testRed",
+      "colourHex": "#ff0000",
+      "merchandisingMedia": [
+        {"id": "test_image_id", "value": "test_image_path"}
+      ],
+      "memory": "20GB"
+    },
+    {"deviceId":"test2b",
+      "displayName": "testPhone1b",
+      "colourName": "testGreen",
+      "colourHex": "#00ff00",
+      "merchandisingMedia": [
+        {"id": "test_image_idb", "value": "test_image_pathb"}
+      ],
+      "memory": "20GB"
+    }
   ]
 };
